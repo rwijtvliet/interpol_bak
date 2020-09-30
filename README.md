dc6555f7d80e2a82ae949e43d77061611129f548
Ruud Wijtvliet <rwijtvliet@gmail.com>
2020-09-30 22:23:46 +0200

    initial (new) commit

---
+++ README.md
@@ -0,0 +1,74 @@
+# interpol
+
+General interpolation inside a polygon, and color maps with >1 input value. Generalisation of linear colormap, which has (at most) 2 extremes, to triangular colormap, which has 3 extremes.
+
+## Interpolation inside a shape: `triangles` and `polygon`
+There are 2 functions to interpolate inside a shape.
+
+* `triangles` tesselates the plane with triangles based on the provided anchorpoints, and does 'standard' barycentric interpolation within each triangle. Disadvantage: a maximum of 3 anchors is used for any point, which does not always look good. Also, extrapolation, i.e. to points that do not lay within the convex hull around the anchorpoints, is not possible. If wanted, the function tries nonetheless, but results are often poor.
+* `polygon` is a more general function, that gives better results. The only drawback is that the anchorpoints must be given in order, as a chain describing the boundary of the polygon - there may be no internal points. 
+
+Here a comparison between the two. The first column shows how the first function interpolates; note how the tesselation triangles are quite visible. The second column shows how the second function interpolates, which is much better. (The dots indicate the anchor points.)
+![image with squares](in_shape_square.png)
+![image with circles](in_shape_circle.png)
+
+## Background: colormaps
+The interpolation functions above can be used for colormaps with 3 axes. For a better understanding, we go into colormaps with 1 and 2 axes first, even though these don't use the interpolation functions.
+
+### With 1 axis
+A common colormap turns any value in the interval [0, 1] into its corresponding color, for example the 'terrain' colormap in matplotlib: 
+
+![cmap](cmap_1axis_notdiverging.png)
+
+Here the values for which a color has been explicitly specified (the anchor points), are marked with dots. The color for any other value is interpolated from the nearest on either side).
+
+If the values that we want to turn into colors don't live on the interval [0, 1], some mapping of the input domain onto [0, 1] is necessary, but this is left out of consideration here.
+
+### With 2 axes, showing relative size: `cmap2`
+If we have a 'diverging' colormap, like the red-green one below, we can see this as a special case of the 1-axis colormap we just saw. But we can also imagine that there are in fact 2 axes (A on the left and B on the right), that meet in the middle. If values for *a* and *b*, both in the interval [0, 1], are provided, we are able to pick a single color on the bar. We do this by taking difference d = b - a. If b is the larger value, we have d > 0, and the color that belongs to this point (a, b) is the color that we find on axis B at the value d. If a is the larger value, we have d < 0, and the corresponding color is found on axis A at the value -d. 
+
+An example would be when a is my expenditure in a given time period, and b is my income in the same time period. The resulting diverging colormap shows the 'income balance':
+
+![cmap for income and expenditure](cmap_2axes_income.png)
+
+(Anchor points are again indicated; here, 2 colors were used on axis A, 3 on axis B, and also the center color was specified.)
+
+It might seem a bit silly to turn what is basically a single variable (*d*) into two variables (*a* and *b*), especially since our color map is really only 1-dimensional. However, this will help us generalise onto three axes, further below. 
+
+It's important that we see that this color map actually displays the *relative size* of *a* and *b*:
+* It shows us, *which is the largest* of the 2 values, from the principle color (red or green) - if *a* is larger, the color comes from axis A. 
+* It shows us, *how much larger* the largest of the two values is - the further the color is towards the end of the axis, the larger their difference.
+
+Note that this is only possible if the values can be meaningfully subtracted from each other. 
+
+Another example might be the time gained by switching between driving and biking to a certain location. Either biking (left) or driving (right) is faster, and the larger the time difference between the two transport options, the further away from the center the color is picked:
+
+![cmap for 2 transport modes](cmap_2axes_transport.png)
+
+Using this final example, we see that one of the values (*a*, *b*) is always 0. That's because we are showing the 'time gained'. The slowest of the 2 options becomes the reference for the other one, i.e., (*a*, *b*) = (something, 0) if the car is the slowest option, which translates to a color from the left side of the color map. Vice versa, when biking is slower, its 'time gained' is 0, so (*a*, *b*) = (0, something). 
+
+Note that this means that the color that corresponds to (bike takes 4 minutes, car takes 7 minutes) is the same as the one that corresponds to (bike takes 5 minutes, car takes 8 minutes), which underlines that this colormap can be used to show *relative size*: the gain by going by bike is 3 minutes in both cases.
+
+The color mapping function, to find the color of a point (*a*, *b*) is returned by `cmap2`. The arguments to this function are the colors on either axis, as well as the one in the middle. It's also possible to include arguments that handle the mapping of the input domain, in (the likely) case the extremes on the axes do *not* correspond to a difference between *a* and *b* of exactly 1.
+
+
+## Colormap with 3 axes: `cmap3`
+Now, what if we have another transport mode in that last example? What if we also have the option of taking public transport, and want to be able to pick a color, based on the differences in time needed to get somewhere by bike, car, or public transport?
+
+Having 3 values means 3 axes: A, B, C. These still meet in a point, but at 120 degree angles:
+
+![3 axes, partial](cmap_3axes_transport_partial.png)
+
+This image, however, only gives us the color when of the 3 values (*a*, *b*, *c*), *two* are zero. For example, when biking has an equal time gain over car and bus, we're in the orange 'arm' of the image using the point (*a*, *b*, *c*) = (something, 0, 0).
+
+In order to also find the colors when only *one* of the coordinates is 0 - remember that at least one of the coordinates will always be 0 - we need to fill in more colors, which is where the interpolation function shown above comes in:
+
+![3 axes, full](cmap_3axes_transport_full.png)
+
+(Here, 2 colors are specified for each of the axes, and none is specified for the center.)
+
+There are many locations in this triangle that indicate a special relationship between the 3 values. In this image, we focus on *a*:
+
+![3 axes, explainer](cmap_3axes_transport_special.png)
+
+The color mapping function, to find the color of a point (*a*, *b*, *c*) is returned by `cmap3`. The arguments to this function are the colors on each axis, as well as the one in the middle. Here, too, it's possible to include arguments that handle the mapping of the input domain.
---
+++ interpol/point2color.py
@@ -0,0 +1,313 @@
+"""
+Color interpolation functions.
+
+2020-09 rwijtvliet@gmail.com
+"""
+
+
+from matplotlib.colors import to_rgb, LinearSegmentedColormap
+import numpy as np
+from typing import Iterable, Callable
+from scipy.spatial import Delaunay, ConvexHull
+
+SR3 = np.sqrt(3)
+
+
+#%% Interpolation functions in single polygon.
+
+def triangles(anchorpoints:Iterable, anchorvalues:Iterable, valoutside=None) -> Callable:
+    """
+    Standard barycentric interpolation function for point inside a single 
+    triangle, or, more generally, for point inside the convex hull around a 
+    set of anchor points that is tesselated with triangles.
+    
+    Triangle has 3 vertices ("anchorpoints" of shape (3, 2)) and 3 values
+    ("anchorvalues", e.g. Iterable of 3 floats, or of 3 colors). When passed 
+    >3 anchorpoints, the area is tesselated with triangles.
+    
+    The returned function takes a point (2-Iterable) and returns the inter-
+    polated value. If the point lies outside the shape, "valoutside" is 
+    returned (if specified) or an attempt at extrapolation is done (default).
+    
+    Source: https://stackoverflow.com/questions/57863618/how-to-vectorize-calculation-of-barycentric-coordinates-in-python
+    """
+    anchorpoints = np.array(anchorpoints)
+    anchorvalues = np.array(anchorvalues)
+    if len(anchorpoints) != len(anchorvalues):
+        raise ValueError("Parameters 'anchorpoints' and 'anchorvalues' must be of equal length.")
+    if len(anchorpoints) < 3:
+        raise ValueError("At least 3 anchorpoints must be specified.")
+    
+    # Tesselate into simplexes (individual triangles).
+    simp = Delaunay(anchorpoints) # each row has indices of the 3 anchorpoints that are the simplex corners.
+    
+    def interp(point):
+        # Find simplex point is in.
+        s = simp.find_simplex(point) #simplex-index that contains point. (-1 means point is in none)
+        if s >= 0: #normal point, inside the hull
+            # Get barycentric coordinates.
+            b0 = simp.transform[s, :2].dot((point - simp.transform[s, 2]))
+            weights = np.array([*b0, 1 - b0.sum()]) # add final coordinate / weight.
+            indices = simp.simplices[s]
+        else: #point outside the hull
+            if valoutside:
+                return valoutside
+            # Find the 2 anchorpoints on the hull line nearest to the point
+            hull = ConvexHull([*anchorpoints, point], qhull_options='QG' + str(len(anchorpoints))) 
+            visible = hull.simplices[hull.good] #lines (anchorpoints) visible from the point
+            for indices in visible: #anchor-indices of visible line
+                p01 = point - anchorpoints[indices] #from line anchors to point
+                lin = anchorpoints[indices[0]] - anchorpoints[indices[1]]
+                dot12 = p01.dot(lin)
+                if np.sign(dot12).sum() == 0: #inside line 'shadow' if one dot product <0, >0
+                    lens = np.linalg.norm(p01, axis=1)
+                    lens = np.abs(dot12)
+                    weights = np.flip(lens) / lens.sum()
+                    break
+            else: #not in shadow of line - use value of nearest anchor.
+                #Find nearest anchor (="anchor 0"). Must be included in 2 lines.
+                indices = list(set(visible.flatten()))
+                sd = ((anchorpoints[indices] - point)**2).sum(axis=1) #squared distance to each anchorpoint
+                indices = [indices[np.argmin(sd)]] #keep only nearest one
+                weights = [1]               
+
+        # Get interpolated value.
+        value = np.dot(anchorvalues[indices].T, weights)
+        return value 
+    return interp
+    
+
+def polygon(anchorpoints:Iterable, anchorvalues:Iterable) -> Callable:
+    """
+    Generalised barycentric interpolation function for point in a polygon.
+    
+    Polygon has n vertices ("anchorpoints" of shape (n, 2)) in right-hand order
+    and n values ("anchorvalues", e.g. iterable of n floats, or np-array of 
+    shape (n, 3) with colors).
+    
+    The returned function takes a point (2-Iterable) and returns the inter-
+    polated value (or extrapolated value if the point lies outside the shape).
+    
+    Source: https://cgvr.cs.uni-bremen.de/papers/barycentric/barycentric.pdf
+    """
+    anchorpoints = np.array(anchorpoints)
+    anchorvalues = np.array(anchorvalues)
+    if len(anchorpoints) != len(anchorvalues):
+        raise ValueError("Parameters 'anchorpoints' and 'anchorvalues' must be of equal length.")
+    if len(anchorpoints) < 3:
+        raise ValueError("At least 3 anchorpoints must be specified.")
+        
+    def interp(point):
+        dv = anchorpoints - point #distance vector to each anchorpoint
+        r = np.linalg.norm(dv, axis=1) #distances to each anchorpoint
+        for i, rr in enumerate(r): 
+            if rr == 0: 
+                return anchorvalues[i] #return anchor value if point is on anchor point
+        dv_next = np.roll(dv, -1, axis=0) # neighbor to each dv
+        A = np.array([np.linalg.det([d1, d2])/2 for d1, d2 in zip(dv, dv_next)]) #determinant of each displ. vector with neighbor
+        D = np.array([np.dot(d1, d2) for d1, d2 in zip(dv, dv_next)])
+        for i, (aa, dd) in enumerate(zip(A, D)):
+            if aa == 0 and dd < 0:
+                j = (i + 1) % len(anchorpoints)
+                return (r[j] * anchorvalues[i] + r[i] * anchorvalues[j]) / (r[i] + r[j])
+        W = 0
+        value = np.zeros(anchorvalues[0].shape)
+        for i, anchorvalue in enumerate(anchorvalues):
+            i_prev = (i-1)%len(anchorvalues)
+            i_next = (i+1)%len(anchorvalues)
+            w = 0
+            if A[i_prev] != 0:
+                w += (r[i_prev] - D[i_prev]/r[i])/A[i_prev]
+            if A[i] != 0:
+                w += (r[i_next] - D[i]/r[i])/A[i]
+            value += anchorvalue * w
+            W += w
+        if W == 0: #no weights - should be impossible
+            return ValueError(f'Point {point} is outside the shape.')
+        return value/W
+    
+    return interp
+
+
+#%% Interpolation on 2 axes.
+
+def cmap2(colorsA:Iterable=[to_rgb('r')], colorsB:Iterable=[to_rgb('g')],
+               colorCenter=None, maxdiff=1, ratio:bool=False) -> Callable:
+    """
+    Create function to find color from 2 float values.
+    
+    Arguments:
+        colorsA is a list of colors, which are used if the value on axis A is 
+        larger than than on axis B. colorCenter, if specified, is the color 
+        used if values on both axes are equal.
+        maxdiff: if the difference (if ratio == False) or the ratio between
+        values is larger than this maximum, the colors are clipped.
+        ratio: specifies if difference (False) or ratio (True, default) between 
+        the values must be calculated. 
+
+    Returns:
+        Function that accepts values a and b, and returns the color that 
+        best fits this datapoint. Smallest value is used as reference for the
+        other.
+    """
+    lst = []
+    for c, v in zip(colorsA, np.linspace(0, 0.5, len(colorsA), False)):
+        lst.append((v, c))
+    if colorCenter:
+        lst.append((0.5, colorCenter))
+    for c, v in zip(np.flip(colorsB, axis=0),
+                    np.flip(np.linspace(1, 0.5, len(colorsB), False))):
+        lst.append((v, c))
+    cmap = LinearSegmentedColormap.from_list('2axes', lst)
+    f = lambda a, b: cmap(0.5 + 0.5*(b - a))
+
+    def color(*ab:float):
+        ab = np.array(ab) / maxdiff #mapping full scale to [0, 1]
+        return f(*ab)
+        
+        # # Turn into correct range: one component being 0, and others maximum of 1.
+        # if ratio:
+        #     minval = np.min(abc)
+        #     if minval <= 0: minval = 0.01
+        #     abc = mapper(abc / minval)
+        # else:
+        #     abc = mapper(abc - np.min(abc))
+        # # 3D to 2D, inside the triangle
+        # times = np.argmax(abc) #index of best value; determines how often to rotate to project onto A or axis a
+        # point = np.linalg.matrix_power(R2, times) \
+        #     @ T @ np.linalg.matrix_power(R3, times) @ abc
+        # return f(point)
+
+    return color
+
+
+#%% Interpolation on 3 axes.
+
+def interp_on_3axes(valuesA:Iterable, valuesB:Iterable, valuesC:Iterable,
+                    valueCenter=None) -> Callable:
+    """
+    Create function for interpolation on three axes, with specified values on 
+    the axes. The axes meet at the center, where a value can also be specified.
+    NB: the word 'value' can be substituted with 'color' if used for color 
+    interpolation.
+    
+    Arguments:
+        valuesA (and valuesB and valuesC) is an Iterable of values. The first
+        value is put at the extreme end of axis A. Other values (if any) are
+        linearly distributed between the end and the center.
+        valueCenter is a single value. If specified, it is put at the center.
+        
+    Returns:
+        Function that accepts values (a, b, c) on each axis, and returns the
+        interpolated value. (a, b, c) should be barycentric coordinates, but 
+        are forced in correct domain if they are not.
+    
+    Implementation details:
+        valueCenter specified:
+            Interpolation in 3 triangular sections, namely (A, B, center), 
+            (B, C, center), (C, A, center). 
+        valueCenter not specified:
+            Interpolation in central triangle with final elements in valuesA, 
+            valuesB, valuesC. If valuesA (or valuesB or valuesC) contains >1
+            element, there exist additional shapes between the corners ABC and 
+            the central triangle, and interpolation is done for these as well.
+        If a shape is triangular and has its value specified only at its corners,
+        'normal' barycentric coordinates are used. Otherwise, 'generalised' bary-
+        centric coordinates are used.
+    """
+    from scipy.spatial import Delaunay
+    valuesABC = np.array([np.array(valuesA), np.array(valuesB), np.array(valuesC)])
+    cornersABC = np.array([(0, 1), (-SR3/2, -0.5), (SR3/2, -0.5)])
+
+    anchors_ax = [[{'point': corner * (1-i/len(values)), 'value': value} 
+                   for i, value in enumerate(values)]
+                  for corner, values in zip(cornersABC, valuesABC)]
+    anchor_ctr = {'point': (0,0), 'value': valueCenter} if valueCenter else None
+    
+    def polygon(*anchors):
+        points = [anchor['point'] for anchor in anchors]
+        values = [anchor['value'] for anchor in anchors]
+        return {'shape': Delaunay(points), 'f': interp_func_polygon(points, values)}
+
+    if anchor_ctr is not None:
+        polys = []
+        for i in range(3):
+            a1, a2 = anchors_ax[(i+1)%3], anchors_ax[i]
+            polys.append(polygon(*a1, anchor_ctr, *np.flip(a2)))
+    else:
+        polys = [polygon(*[anchors[-1] for anchors in anchors_ax])] #central triangle
+        for i in range(3):
+            a1, a2 = anchors_ax[(i+1)%3], anchors_ax[i]
+            if len(a1) > 1 or len(a2) > 1:
+                polys.append(polygon(*a1, *np.flip(a2))) #outside shapes (if any)
+        
+    def interp(*abc:float):
+        abc = np.array(abc)
+        abc = abc - abc.min() #does not change location of point
+        if abc.sum() > 1:
+            abc = abc / abc.sum() #make sure it stays inside triangle
+        point = abc.dot(cornersABC)
+        for p in polys:
+            if p['shape'].find_simplex(point) > -1:
+                return p['f'](point)
+        return None #should never happen
+    
+    return interp
+
+
+def cmap3(colorsA:Iterable=[to_rgb('r')], colorsB:Iterable=[to_rgb('g')], 
+               colorsC:Iterable=[to_rgb('b')], colorCenter=None, 
+               maxdiff=1, ratio:bool=False) -> Callable:
+    """
+    Create function to find color from 3 float values.
+    
+    Arguments:
+        colorsA is a list of colors, which are used if the value on axis a is 
+        largest (and those on b and c are equal). In other cases, there is an 
+        interpolation between the colors of the axes with the largest values.
+        colorCenter, if specified, is the color used if values on all axes are 
+        equal.
+        maxdiff: if the difference (if ratio == False) or the ratio between
+        values is larger than this maximum, the colors are clipped.
+        ratio: specifies if difference (False) or ratio (True, default) between 
+        the values must be calculated. 
+
+    Returns:
+        Function that accepts values a, b, and c, and returns the color that 
+        best fits this datapoint. Smallest value is used as reference for the
+        other ones.
+    """
+    f = interp_on_3axes(colorsA, colorsB, colorsC, colorCenter)
+    mindiff = 1 if ratio else 0
+    # mapper = interp1d((mindiff, maxdiff), (0, 1), bounds_error=False, fill_value=(0, 1))
+
+    # R2 = np.array([[-1, -SR3/2],
+    #                 [SR3/2,  -1]]) #rotate 120 degrees in 2D (i.e. A->B->C)
+    # R3 = np.array([[0, 1, 0],
+    #                 [0, 0, 1],
+    #                 [1, 0, 0]]) #rotate through axes in 3D a<-b<-c
+    # T = np.array([[0, -SR3/4, SR3/4],
+    #               [1,   -3/4,  -3/4]]) # (a, b, c) to (x, y)
+    
+    def color(*abc:float):
+        abc = np.array(abc) / maxdiff #mapping full scale to [0, 1]
+        return f(*abc)
+        
+        # # Turn into correct range: one component being 0, and others maximum of 1.
+        # if ratio:
+        #     minval = np.min(abc)
+        #     if minval <= 0: minval = 0.01
+        #     abc = mapper(abc / minval)
+        # else:
+        #     abc = mapper(abc - np.min(abc))
+        # # 3D to 2D, inside the triangle
+        # times = np.argmax(abc) #index of best value; determines how often to rotate to project onto A or axis a
+        # point = np.linalg.matrix_power(R2, times) \
+        #     @ T @ np.linalg.matrix_power(R3, times) @ abc
+        # return f(point)
+
+    return color
+
+
+
+    
